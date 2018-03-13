b79d0bad75b673b37549af81de36c146a0b74813
jsundahl <jsundahl@onshape.com>
2018-03-12 22:01:23 -0400

    everything is fine.

---
+++ jobs/everythingIsFine.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  name: 'everythingIsFine',
+  description: 'everything is fine',
+  type: 'responsive',
+  dependencies: [
+  ],
+  hiddenFromHelp: true,
+  phrases: [
+    'are you ok',
+    'is everything ok'
+  ],
+  fn: function ({
+                  bot,
+                  message
+                }) {
+    bot.reply(message, 'e̷̵̺̫̦̳̠͉͎ͦ̎͘v̝̠͎̮̩̯̺̗͊̉ẽ̵̳̠̺͕̼̗͂͆ṟ̫͉̓́ȳ̗̤̩̘̣̻͚̚͠ͅţ̭̤̲͉͑̋̅͘h̡̠̟͕̭̳̼̋͌̂͊̏̽̅̚i̜̗̰̞͊̽̽̈̅n̼̲͇̭̅ͣͥg͉̳̹̪ͮ̿ͩ ̻͚̠͓̤̣̝͐͠͝i̪͓̗̫͕̝̰͒ͥͭͩ̕͡͝s͓̞͍̝̱̭͉̲͆̂̐͌̇͑͗̂ ͈͉̣̮̾ͪ́ͤ̾f̢̨̭͙̬̑͑̅̀́̿i̻̼̘͈̤̞ͩͮͯͯ͆n̳̭͕̩͔̟̮ͨ̎̀̒̾̎ͦͤͮ̀ê̡̘̲̄͊͑');
+  }
+};
