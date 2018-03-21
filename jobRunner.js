c38e510527b7b22c13f17d09cf9b3aabe5312456
jsundahl <jsundahl@onshape.com>
2018-03-21 19:16:32 -0400

    add ability to kill program from slack

--- jobRunner.js
+++ jobRunner.js
@@ -71,6 +71,10 @@ function bundleDependencies(dependenciesObj, jobDependencies, optionalSlackChann
 
 function listenForSlackMessages(dependenciesObj) {
   return async (bot, message) => {
+    //exit if we get the restart command
+    if (message.text === '//restart') {
+      process.exit(0)
+    }
     // find the job this message pertains to
     const [job, phraseMatch] = findJobFromMessage(message);
     // check if it needs identifying info
